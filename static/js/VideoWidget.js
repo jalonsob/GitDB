0e91d74df0ea3dfb9c8c987ca598097e30ca2c84
jalonsob <jalonsob1991@gmail.com>
2015-05-04 10:17:44 +0200

    update

--- static/js/VideoWidget.js
+++ static/js/VideoWidget.js
@@ -1,7 +1,13 @@
 //Widget oriented to make a charts with Highcharts
 
 function VideoWidget(id,panel,color,direction,content,width,height,x,y){
-	Widget.call(this,id,panel,color,width,height)
+	if((width==undefined) && (height==undefined)){
+		Widget.call(this,id,panel,color,12,9)
+
+	}else{
+		Widget.call(this,id,panel,color,width,height)
+
+	}
 	this.buttons='<button onclick="deleteWidget('+this.panel+','+this.id+')" type="button" class="btn btn-xs btn-default">Delete</button><button onclick="ChangePanelMenu('+this.id+')" type="button" class="btn btn-xs btn-default">Move to</button><button onclick="ShowValuesGraph('+this.id+')" type="button" class="btn btn-xs btn-default">Settings</button>'
 	this.content=content||'<img id="load'+id+'" src="/templates/images/cargando.gif" height="42" width="42">'
 	this.square='<div id= "widget'+this.id+'" class="panel panel-primary widgetDrop" style="border-style: groove;border-color: black;border-width: 3px"><div id="header'+this.id+'" class="panel-heading" style="background-color:'+this.color+'">'+this.buttons+'</div><div id="'+this.id+'" class="panel-body">'+this.content+'</div></div>';
@@ -39,7 +45,7 @@ function VideoWidget(id,panel,color,direction,content,width,height,x,y){
 			$("#currentCreation").remove();
 		}
 		$("#conten").append('<div id="currentCreation">')
-		$("#currentCreation").append('<p>With : <input  width="5" placeholder="'+this.gridsterWidth+'" id="x" ></p> <p>Heigh: <input width="5" placeholder="'+this.gridsterheight+'" id="y"></div></p>')
+		$("#currentCreation").append('<p>With : <input  width="5" placeholder="'+12+'" id="x" ></p> <p>Heigh: <input width="5" placeholder="'+9+'" id="y"></div></p>')
 		$("#currentCreation").append('<p>Playing <input id="play" type="checkbox"> Url : <input id="url" class="form-control"></p>')
 		$("#currentCreation").append('<button onclick="FillWidget('+id+')" type="button" class="btn btn-xs btn-default">Create</button>')
 		$("#currentCreation").append('<button onclick="deleteCreation('+id+')" type="button" class="btn btn-xs btn-default">Cancel</button>')
@@ -72,7 +78,7 @@ function VideoWidget(id,panel,color,direction,content,width,height,x,y){
 			if($('#play').is(':checked')){
 				direction=direction+"?autoplay=1"
 			}
-			this.content='<iframe src="'+direction+'" width="'+(this.gridsterWidth*27)+'" height="'+(this.gridsterheight*23)+'" frameborder="0" allowfullscreen></iframe>'
+			this.content='<iframe src="'+direction+'" width="'+(this.gridsterWidth*28)+'" height="'+(this.gridsterheight*23)+'" frameborder="0" allowfullscreen></iframe>'
 			this.square='<div id= "widget'+this.id+'" class="panel panel-primary widgetDrop" style="border-style: groove;border-color: black;border-width: 3px"><div id="header'+this.id+'" class="panel-heading" style="background-color:'+this.color+'">'+this.buttons+'</div><div id="'+this.id+'" class="panel-body">'+this.content+'</div></div>';
 
 			this.toDraw=true;
@@ -121,7 +127,7 @@ function VideoWidget(id,panel,color,direction,content,width,height,x,y){
 			  $("#currentSettings").remove();
 		}
 		$("#conten").append('<div id="currentSettings">')
-		$("#currentSettings").append('<p>With : <input  width="10" placeholder="'+this.gridsterWidth+'" id="x" > Heigh: <input width="10" placeholder="'+this.gridsterheight+'" id="y"></div></p>')
+		$("#currentSettings").append('<p>With : <input  width="10" placeholder="'+12+'" id="x" > Heigh: <input width="10" placeholder="'+9+'" id="y"></div></p>')
 		$("#currentSettings").append('<p>Playing <input id="play" type="checkbox"> Url : <input id="url" class="form-control" placeholder="'+this.direction+'"></p>')
 		$("#currentSettings").append('<button onclick="ChangeValuesGraph('+this.id+')" type="button" class="btn btn-xs btn-default">Create</button>')
 		$("#currentSettings").append('<button onclick="deleteSettings()" type="button" class="btn btn-xs btn-default">Cancel</button>')
