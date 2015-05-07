704eaea9db43ac6cda5f4c8de951ad2e84554026
jalonsob <jalonsob1991@gmail.com>
2015-05-07 13:03:54 +0200

    updated

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -357,10 +357,9 @@ $(document).ready(function() {
             repos.forEach(function(element){
               $("#listRepo").append('<p><input name="repos" value="'+element.name+'" type="radio">   '+element.name+'</p>')
             })
-            send=JSON.stringify(info);
-            console.log(send)
+
             $("#panelSaveConten").append('<p>Save as...</p><p></p><p><input id="fileName" class="form-control"></div></p>')
-            $("#panelSaveConten").append('<button onclick="Save('+"'"+send+"'"+')" type="button" class="btn btn-xs btn-default">Save</button>')
+            $("#panelSaveConten").append('<button onclick="Save('+info+')" type="button" class="btn btn-xs btn-default">Save</button>')
             $("#panelSaveConten").append('<button onclick="CancelSave()" type="button" class="btn btn-xs btn-default">Cancel</button>')
           });
         }else{
